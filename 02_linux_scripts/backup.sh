02e49bd8337c862387bd10e85473cc8031d80da2
loranvimal21 <144791263+loranvimal21@users.noreply.github.com>
2025-12-09 19:13:08 +0530

    Update backup.sh

--- 02_linux_scripts/backup.sh
+++ 02_linux_scripts/backup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-# Simple backup script
-cp -r /home /backup
+
+# This script prints a message (simple example)
+echo "Backup started..."
+
+# Pretend backup (for demo)
+echo "Copying files from /home to /backup..."
+
+# Script end
 echo "Backup completed!"
+
